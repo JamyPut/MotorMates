c290b45fbe47162fa840fa108f8bc2f2758bff42
JamyPut <jp2001dj@gmail.com>
2023-07-05 18:10:46 +0200

    Setup springboot application and dependencies.

---
+++ src/main/java/com/MotorMates/MotorMates/MotorMatesApplication.java
@@ -0,0 +1,13 @@
+package com.MotorMates.MotorMates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorMatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorMatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MotorMates/MotorMates/MotorMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MotorMates.MotorMates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorMatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
