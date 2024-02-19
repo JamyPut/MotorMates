3c227ce86bde57957b3d34d28cda2933267977f2
JamyPut <jp2001dj@gmail.com>
2024-02-19 23:38:58 +0100

    added jwt dependencies

--- src/main/java/com/MotorMates/MotorMates/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/MotorMates/MotorMates/configuration/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.MotorMates.MotorMates.configuration;
 
+import com.MotorMates.MotorMates.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,8 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -22,10 +25,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String loginEmail;
         if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
+        loginEmail = jwtService.extractLoginMail(jwt);
     }
 }
