1407e41961ea59eb5cc03d0b907370256e39af6b
JamyPut <jp2001dj@gmail.com>
2024-02-20 15:31:59 +0100

    implemented secret key

--- src/main/java/com/MotorMates/MotorMates/service/JwtService.java
+++ src/main/java/com/MotorMates/MotorMates/service/JwtService.java
@@ -1,10 +1,32 @@
 package com.MotorMates.MotorMates.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "34663330314551764979634C5152316B6C335552305962525144485A77485758";
     public String extractLoginMail(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
