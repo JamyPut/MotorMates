bcb8bdfed58ab16cbfeceac459ef98bddadebd5b
JamyPut <jp2001dj@gmail.com>
2024-02-20 23:23:12 +0100

    fixed dependencies and started implementing auth controller

---
+++ src/main/java/com/MotorMates/MotorMates/Authentication/AuthenticationController.java
@@ -0,0 +1,21 @@
+package com.MotorMates.MotorMates.Authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/forum/auth")
+@RequiredArgsConstructor
+ class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+        // 1:40
+    }
+}
